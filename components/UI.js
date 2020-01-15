c977a62ce29ec5522ac18d6a3746dd6b008d5622
Andrew Pang <thpangab@gmail.com>
2020-01-16 00:53:33 +0800

    Update linting

--- components/UI.js
+++ components/UI.js
@@ -3,10 +3,18 @@ import PropTypes from 'prop-types';
 // import styled from 'styled-components/native';
 import {Colors} from '../screens/Styles';
 
-import { StyleSheet, TouchableHighlight, View, Text, Platform, Image as RNImage, ImageBackground as RNImageBackground, useEffect } from 'react-native';
-import { TextInput, HelperText } from "react-native-paper";
+import {
+  StyleSheet,
+  TouchableHighlight,
+  View,
+  Text,
+  Platform,
+  Image as RNImage,
+  ImageBackground as RNImageBackground,
+  useEffect,
+} from 'react-native';
+import {TextInput, HelperText} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 
 const styles = StyleSheet.create({
   inputsContainer: {
@@ -14,7 +22,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 10,
     margin: 4,
-    flex: 1
+    flex: 1,
   },
   button: {
     width: '80%',
@@ -24,14 +32,13 @@ const styles = StyleSheet.create({
 
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonText: {
     fontSize: 20,
-    color: 'white'
-  }
+    color: 'white',
+  },
 });
-
 
 // export const Header = styled.Text`
 //   font-size: 30;
@@ -50,22 +57,29 @@ const styles = StyleSheet.create({
 // `;
 
 export const Input = props => {
-
-  return <React.Fragment>
-    <TextInput
-    {...props}
-    mode="flat"
-    label={props.binding.label[props.name] + (props.required != null && props.binding.data[props.name] == null ? ' ' + props.required : '')}
-    value={props.binding.data[props.name]}
-    onChangeText={value => {props.binding.onChange(props.name, value)}}
-    />
-    {props.binding.helperText[props.name] ? (
-      <HelperText type="error" visible={true}>
-        {props.binding.helperText[props.name]}
-      </HelperText>
-    ) : null}
-
-  </React.Fragment>
+  return (
+    <React.Fragment>
+      <TextInput
+        {...props}
+        mode="flat"
+        label={
+          props.binding.label[props.name] +
+          (props.required != null && props.binding.data[props.name] == null
+            ? ' ' + props.required
+            : '')
+        }
+        value={props.binding.data[props.name]}
+        onChangeText={value => {
+          props.binding.onChange(props.name, value);
+        }}
+      />
+      {props.binding.helperText[props.name] ? (
+        <HelperText type="error" visible={true}>
+          {props.binding.helperText[props.name]}
+        </HelperText>
+      ) : null}
+    </React.Fragment>
+  );
 };
 
 export class MiniButton extends React.Component {
@@ -76,9 +90,9 @@ export class MiniButton extends React.Component {
           margin: 0,
           borderWidth: 1,
           borderColor: '#1A3F5D',
-          borderRadius: 0
+          borderRadius: 0,
         }}
-        buttonStyle={{ width: '100%', height: '100%', borderRadius: 0 }}
+        buttonStyle={{width: '100%', height: '100%', borderRadius: 0}}
         {...this.props}
       />
     );
@@ -88,26 +102,24 @@ export class MiniButton extends React.Component {
 export class Button extends React.Component {
   render() {
     return (
-      <View style={{ ...styles.inputsContainer, ...this.props.style }}>
+      <View style={{...styles.inputsContainer, ...this.props.style}}>
         <TouchableHighlight
-          underlayColor = {this.props.color}
+          underlayColor={this.props.color}
           style={{
             ...styles.button,
             ...this.props.buttonStyle,
-            backgroundColor: this.props.color
+            backgroundColor: this.props.color,
           }}
           onPress={() => {
             this.props.onPress();
-          }}
-        >
+          }}>
           <Text
             style={{
               ...styles.buttonText,
               ...this.props.textStyle,
               color: this.props.fontColor,
-              fontSize: this.props.fontSize
-            }}
-          >
+              fontSize: this.props.fontSize,
+            }}>
             {this.props.title}
           </Text>
         </TouchableHighlight>
@@ -118,34 +130,39 @@ export class Button extends React.Component {
 
 Button.propTypes = {
   color: PropTypes.string,
-  fontColor: PropTypes.string
+  fontColor: PropTypes.string,
 };
 
 Button.defaultProps = {
   color: '#225599',
-  fontColor: 'white'
+  fontColor: 'white',
 };
 
-
-export const HeaderButton = (props) => {
-
-  return (<Button
-    {...props}
-    color = {Colors.main}
-    fontSize={20}
-    buttonStyle={{borderBottomWidth: props.active ? 2 : 0, width: 30, height: 40, paddingBottom: 4, borderColor: 'white', borderRadius: 0, ...(props.buttonStyle || {}) }}
-    textStyle={{ width: '100%', ...(props.textStyle || {}) }}
-     />
+export const HeaderButton = props => {
+  return (
+    <Button
+      {...props}
+      color={Colors.main}
+      fontSize={20}
+      buttonStyle={{
+        borderBottomWidth: props.active ? 2 : 0,
+        width: 30,
+        height: 40,
+        paddingBottom: 4,
+        borderColor: 'white',
+        borderRadius: 0,
+        ...(props.buttonStyle || {}),
+      }}
+      textStyle={{width: '100%', ...(props.textStyle || {})}}
+    />
   );
-}
-
-
+};
 
 export class ToggleButton extends React.Component {
-  state = { on: false };
+  state = {on: false};
 
   UNSAFE_componentWillMount() {
-    this.setState({ on: this.props.on });
+    this.setState({on: this.props.on});
   }
 
   render() {
@@ -156,31 +173,39 @@ export class ToggleButton extends React.Component {
         onPress={() => {
           let on = !this.state.on;
           this.props.onValueUpdate(on, this.props.index);
-          this.setState({ on });
+          this.setState({on});
         }}
         {...this.props}
-        buttonStyle={{ width: '100%', height: '100%', borderRadius: 0,...this.props.buttonStyle }}
+        buttonStyle={{
+          width: '100%',
+          height: '100%',
+          borderRadius: 0,
+          ...this.props.buttonStyle,
+        }}
         style={{
           margin: 0,
           borderWidth: 1,
           borderColor: '#1A3F5D',
           borderRadius: 0,
-          ...this.props.style
+          ...this.props.style,
         }}
       />
     );
   }
   static defaultProps = {
-    onValueUpdate: () => {}
+    onValueUpdate: () => {},
   };
 }
 
 export class SlotButton extends React.Component {
   render() {
-    return <ToggleButton {...this.props} 
-    style={{borderWidth: 1, borderRadius: 10, margin: 4}} 
-    buttonStyle={{borderRadius: 10}} 
-    />;
+    return (
+      <ToggleButton
+        {...this.props}
+        style={{borderWidth: 1, borderRadius: 10, margin: 4}}
+        buttonStyle={{borderRadius: 10}}
+      />
+    );
   }
 }
 
@@ -188,120 +213,121 @@ let RNFS = require('react-native-fs');
 import shorthash from 'shorthash';
 
 export class Image extends React.Component {
-  state = { source:require('../room.png') };
-  loadFile = ( path )=> {
-    this.setState({ source:{uri:path}}) ;
-  }
-  downloadFile = (uri,path) => {
+  state = {source: require('../room.png')};
+  loadFile = path => {
+    this.setState({source: {uri: path}});
+  };
+  downloadFile = (uri, path) => {
     // consol.warn('cached', path)
     if (uri.substr(0, 4) == 'http') {
-      RNFS.downloadFile({fromUrl:uri, toFile: path}).promise
-        .then(res =>
-          {
-            this.loadFile(path);
-            // consol.warn('cached', path)
-          });
+      RNFS.downloadFile({fromUrl: uri, toFile: path}).promise.then(res => {
+        this.loadFile(path);
+        // consol.warn('cached', path)
+      });
+    } else {
+      this.setState({source: {uri}});
     }
-    else {
-      this.setState({source: {uri}})
-    }
-  }
+  };
 
-  UNSAFE_componentWillMount(){
+  UNSAFE_componentWillMount() {
     // console.warn(this.props.source)
     // console.warn('mount', this.props)
     if (this.props.direct == true && this.props.source.uri != null) {
       // console.warn(this.props.source)
-      this.setState({source: this.props.source})
-    }
-    else {
-      let uri = this.props.source.url ; 
+      this.setState({source: this.props.source});
+    } else {
+      let uri = this.props.source.url;
       uri = uri || '';
       if (uri != '') {
         // uri = 'img/room.png';
-        
+
         const name = shorthash.unique(uri);
-        const extension = (Platform.OS === 'android') ? 'file://' : '' 
-        const path =`${extension}${RNFS.CachesDirectoryPath}/${name}.png`;
-        RNFS.exists(path).then( exists => {
-            if(exists)this.loadFile(path) ;
-            else this.downloadFile(uri,path) ;
-          });
+        const extension = Platform.OS === 'android' ? 'file://' : '';
+        const path = `${extension}${RNFS.CachesDirectoryPath}/${name}.png`;
+        RNFS.exists(path).then(exists => {
+          if (exists) this.loadFile(path);
+          else this.downloadFile(uri, path);
+        });
       }
     }
   }
 
   render() {
-    return(
+    return (
       <TouchableHighlight onPress={this.props.onPress}>
-        <RNImage {...this.props} source={this.state.source}/>
+        <RNImage {...this.props} source={this.state.source} />
       </TouchableHighlight>
-   );
+    );
   }
 }
 
 export class ImageBackground extends React.Component {
-  state = { source:require('../room.png') }
-  loadFile = ( path )=> {
-    this.setState({ source:{uri:path}}) ;
-  }
-  downloadFile = (uri,path) => {
+  state = {source: require('../room.png')};
+  loadFile = path => {
+    this.setState({source: {uri: path}});
+  };
+  downloadFile = (uri, path) => {
     // consol.warn('cached', path)
     if (uri.substr(0, 4) == 'http') {
-      RNFS.downloadFile({fromUrl:uri, toFile: path}).promise
-        .then(res =>
-          {
-            this.loadFile(path);
-            // consol.warn('cached', path)
-          });
+      RNFS.downloadFile({fromUrl: uri, toFile: path}).promise.then(res => {
+        this.loadFile(path);
+        // consol.warn('cached', path)
+      });
+    } else {
+      this.setState({source: {uri}});
     }
-    else {
-      this.setState({source: {uri}})
-    }
-  }
+  };
 
-  UNSAFE_componentWillMount(){
+  UNSAFE_componentWillMount() {
     // console.warn(this.props.source)
     // console.warn('mount', this.props)
     if (this.props.direct == true && this.props.source.uri != null) {
       // console.warn(this.props.source)
-      this.setState({source: this.props.source})
-    }
-    else {
-      let uri = this.props.source.url ; 
+      this.setState({source: this.props.source});
+    } else {
+      let uri = this.props.source.url;
       uri = uri || '';
       if (uri != '') {
         // uri = 'img/room.png';
-        
+
         const name = shorthash.unique(uri);
-        const extension = (Platform.OS === 'android') ? 'file://' : '' 
-        const path =`${extension}${RNFS.CachesDirectoryPath}/${name}.png`;
-        RNFS.exists(path).then( exists => {
-            if(exists)this.loadFile(path) ;
-            else this.downloadFile(uri,path) ;
-          });
+        const extension = Platform.OS === 'android' ? 'file://' : '';
+        const path = `${extension}${RNFS.CachesDirectoryPath}/${name}.png`;
+        RNFS.exists(path).then(exists => {
+          if (exists) this.loadFile(path);
+          else this.downloadFile(uri, path);
+        });
       }
     }
   }
 
   render() {
-    return(
+    return (
       <TouchableHighlight onPress={this.props.onPress}>
-        <RNImageBackground {...this.props} source={this.state.source}/>
+        <RNImageBackground {...this.props} source={this.state.source} />
       </TouchableHighlight>
-   );
+    );
   }
 }
 
-export const OptionSelect = (props) => {
-  
-    return (
+export const OptionSelect = props => {
+  return (
     <TouchableHighlight onPress={props.onPress}>
-    <View style={{flex: 1, flexDirection: 'row', backgroundColor: '#E7E7E7', borderColor: '#ADADAD', borderBottomWidth: 1, height: 58, padding: 14, alignItems: 'center', justifyContent: 'space-between'}}>
-      <Text style={{fontSize: 16, color: '#6A6A6A'}}>{props.children}</Text>
-      <Icon name="angle-right" size={24} color="#C8C7CC" />
-    </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          backgroundColor: '#E7E7E7',
+          borderColor: '#ADADAD',
+          borderBottomWidth: 1,
+          height: 58,
+          padding: 14,
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <Text style={{fontSize: 16, color: '#6A6A6A'}}>{props.children}</Text>
+        <Icon name="angle-right" size={24} color="#C8C7CC" />
+      </View>
     </TouchableHighlight>
-    );
-}
-
+  );
+};
--- screens/Styles.js
+++ screens/Styles.js
@@ -1,5 +1,4 @@
-import { StyleSheet } from 'react-native';
-
+import {StyleSheet} from 'react-native';
 
 // 預設顏色：R=37  G=96  B=164
 // 背景：R=255  G=255  B=255
@@ -36,50 +35,49 @@ export const Colors = {
   menuText: colorMenuText,
   menuSelected: colorMenuSelected,
   menuSelectedText: colorMenuSelectedText,
-  navButton: colorNavButton
+  navButton: colorNavButton,
 };
-
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     // padding: 8,
     backgroundColor: 'white',
-    height: '100%'
+    height: '100%',
   },
   title: {
     padding: 8,
-    lineHeight: 26
+    lineHeight: 26,
   },
   headerBar: {
     height: 18,
     marginBottom: 10,
-    backgroundColor: '#3568A6'
+    backgroundColor: '#3568A6',
   },
   blackTitle: {
-    color: 'white'
+    color: 'white',
   },
   blackText: {
-    color: 'white'
+    color: 'white',
   },
   blackContainer: {
     flex: 1,
     // padding: 8,
     backgroundColor: 'black',
     color: 'white',
-    height: '100%'
+    height: '100%',
   },
   container2: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
     flexGrow: 1,
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
     // margin: 10
   },
   padding: {
@@ -89,21 +87,21 @@ export default StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'stretch',
-    margin: 10
+    margin: 10,
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10
+    margin: 10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5
+    marginBottom: 5,
   },
   safeArea: {
     flex: 1,
-    backgroundColor: colorNavButton
+    backgroundColor: colorNavButton,
   },
   fixBackground: {
     backgroundColor: colorNavButton,
@@ -112,20 +110,20 @@ export default StyleSheet.create({
     right: 0,
     left: 0,
     height: 100,
-    zIndex: -1000
+    zIndex: -1000,
   },
   centerScreen: {
     alignItems: 'center',
-    justifyContent: 'center', 
-    height: 200
+    justifyContent: 'center',
+    height: 200,
   },
   welcome: {
-    fontSize: 20
+    fontSize: 20,
   },
   bigName: {
     fontSize: 30,
     textAlign: 'center',
-    color: colorNavButton
+    color: colorNavButton,
   },
   scrollView: {
     // height: "100%"
@@ -135,5 +133,5 @@ export default StyleSheet.create({
     // height: "100%"
     // borderWidth: 1,
     // borderColor: "red"
-  }
+  },
 });
