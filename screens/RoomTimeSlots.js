6a76c50e12d80dffdee1e8453f1456435d2887dc
Andrew Pang <thpangab@gmail.com>
2020-01-24 00:23:28 +0800

    Update room details time selection ui

--- screens/RoomTimeSlots.js
+++ screens/RoomTimeSlots.js
@@ -2,7 +2,7 @@ import NavigationService from '../NavigationService';
 
 import React, { useState, useEffect } from 'react';
 import { useStateValue } from '../state';
-import { View, Text, StyleSheet, Alert, Animated } from 'react-native';
+import { View, Text, StyleSheet, Animated, Dimensions } from 'react-native';
 import { Image } from '../components/UI';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 import {
@@ -15,11 +15,13 @@ import {
   Paragraph,
   Divider,
   Portal,
-  Modal
+  Modal,
 } from 'react-native-paper';
+import Carousel from 'react-native-snap-carousel';
 import { KeyboardAwareScrollView as ScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { getSlots, yymmdd, message, alert } from '../utils';
 import styles from './Styles';
+import { Colors } from './Styles';
 import { Button as UIButton } from '../components/UI';
 
 const Screen = props => {
@@ -33,7 +35,7 @@ const Screen = props => {
     prices: {},
     isOpen: true,
     today: new Date(),
-    maxDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 90)
+    maxDate: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 90),
   });
 
   const d = new Date();
@@ -53,7 +55,7 @@ const Screen = props => {
     let room = props.navigation.state.params.room;
     // console.warn(new Date(d).getDay());
     getSlots(room.id, date).then(({ slots, prices }) => {
-      console.log(prices)
+      console.log(prices);
       timeslots = slots || [];
       setState({ ...state, timeslots, prices, booked: {} });
       let isInCart = {};
@@ -107,7 +109,6 @@ const Screen = props => {
       }
       setState({ ...state, slots: [] });
     }
-
   }
 
   const onChooseDateButton = () => {
@@ -124,165 +125,279 @@ const Screen = props => {
     hideCal();
   }
 
-  let k = 0;
+  const sliderWidth = Dimensions.get('window').width;
+  const {
+    address,
+    area,
+    name: roomName,
+    photos: carouselBanner,
+    pricePerHour,
+  } = props.navigation.state.params.room;
   return (
-    <View style={style.container}>
-      <View style={{flex: 0}}>
-        <Text style={style.roomname}>
-          {props.navigation.state.params.room.name}
-        </Text>
-        <Text style={style.header2}>請選擇日期</Text>
-        <Button
-          mode="contained"
-          onPress={onChooseDateButton}
-          style={{ marginTop: 8 }}
-        >
-          {selectedDate}
-        </Button>
-        <Portal>
-          <Modal visible={state.isCalShow} onDismiss={hideCal}>
-            <Calendar
-              onDayPress={day => {
-                onSelectDate(day.dateString);
-              }}
-              markedDates={{
-                [selectedDate]: { selected: true }
-              }}
-              minDate={state.today}
-              maxDate={state.maxDate}
-              style={style.calendar}
-              theme={{
-                todayTextColor: '#2d4150'
-              }}
-            />
-          </Modal>
-        </Portal>
-      </View>
-      <View style={{flex: 1, flexGrowth: 1}}>
-          <Text style={{ ...style.header2, marginTop: 30 }}>請選擇時間</Text>
-          <ScrollView>
-          {state.isCustom ? (<Text>是日有特別安排</Text>) : null}
-          {state.isOpen ? (
-              ['am', 'pm'].map(ampm => (
-                <View key={ampm} style={style.half}>
-                  <Text style={style.ampm}>
-                    {ampm == 'am' ? '' : ''}
-                  </Text>
-                  <Divider />
-                  <View style={{}}>
-                    {Object.keys(state.timeslots)
-                      .filter(
-                        x => (ampm == 'pm') != state.timeslots[x] < '12:00'
-                      )
-                      .map(k => (
-                        <Text
-                          key={'dummy' + state.timeslots[k]}
-                          onPress={selectSlot.bind(this, state.timeslots[k])}
-                          style={{
-                            ...style.slottext,
-                            color:
-                              state.booked[state.timeslots[k]] ||
-                                state.isInCart[state.timeslots[k]]
-                                ? 'gray'
-                                : state.slots[state.timeslots[k]]
-                                  ? 'red'
-                                  : '#225599'
-                          }}
-                        >
-                          {state.timeslots[k] + ' - $' + (state.prices[state.timeslots[k]] / 2)}
-                        </Text>
-                      ))}
-                  </View>
-                </View>
-              ))
-            
-          ) : (
-              <View>
-                <Text>是日休息</Text>
-              </View>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View styles={{ height: '100%' }}>
+          <Carousel
+            autoplay={true}
+            loop={true}
+            data={carouselBanner}
+            sliderWidth={sliderWidth}
+            itemWidth={sliderWidth}
+            renderItem={({ item, index }) => (
+              <Animated.View
+                style={{
+                  width: '100%',
+                  height: 200,
+                  opacity: state.opacity,
+                }}>
+                <Image
+                  source={{
+                    url: item,
+                  }}
+                  style={{
+                    width: '100%',
+                    height: 300,
+                  }}
+                  onLoadStart={e => {
+                    state.opacity.setValue(0);
+                  }}
+                  onLoadEnd={e => {
+                    Animated.timing(state.opacity, {
+                      toValue: 1,
+                      duration: 500,
+                      useNativeDriver: true,
+                    }).start();
+                  }}
+                />
+              </Animated.View>
             )}
-                  </ScrollView>
-
+          />
+          <View style={style.header}>
+            <Headline style={style.name}>{roomName}</Headline>
+            <Text style={style.address}>{address}</Text>
+            <Text style={style.price}>
+              {`時租$${pricePerHour} - ${area}尺空間`}
+            </Text>
+          </View>
+          <View style={style.datePickerContainer}>
+            <Text style={style.subtitle}>請選擇日期</Text>
+            <Button
+              mode="outlined"
+              color="black"
+              onPress={onChooseDateButton}
+              style={{ marginTop: 8, borderColor: 'black' }}
+              theme="red">
+              <Text style={{ fontSize: 12 }}>{selectedDate}</Text>
+            </Button>
+            <Portal>
+              <Modal visible={state.isCalShow} onDismiss={hideCal}>
+                <Calendar
+                  onDayPress={day => {
+                    onSelectDate(day.dateString);
+                  }}
+                  markedDates={{
+                    [selectedDate]: { selected: true },
+                  }}
+                  minDate={state.today}
+                  maxDate={state.maxDate}
+                  style={style.calendar}
+                  theme={{
+                    todayTextColor: '#2d4150',
+                  }}
+                />
+              </Modal>
+            </Portal>
+          </View>
+          <View style={style.timePickerContainer}>
+            <Text
+              style={{
+                ...style.subtitle,
+                marginHorizontal: 30,
+                marginBottom: 5,
+              }}>
+              請選擇時間
+            </Text>
+            <ScrollView>
+              {state.isCustom ? (
+                <Text style={style.specialArrangementText}>是日有特別安排</Text>
+              ) : null}
+              {state.isOpen ? (
+                ['am', 'pm'].map(ampm => (
+                  <View key={ampm} style={style.half}>
+                    <Text style={style.ampm}>
+                      {ampm == 'am' ? '上午' : '下午'}
+                    </Text>
+                    <View style={style.timePickerOptions}>
+                      {Object.keys(state.timeslots)
+                        .filter(
+                          x => (ampm == 'pm') != state.timeslots[x] < '12:00',
+                        )
+                        .map(k => (
+                          <Text
+                            key={'dummy' + state.timeslots[k]}
+                            onPress={selectSlot.bind(this, state.timeslots[k])}
+                            style={{
+                              ...style.slottext,
+                              backgroundColor:
+                                state.booked[state.timeslots[k]] ||
+                                state.isInCart[state.timeslots[k]]
+                                  ? 'gray'
+                                  : state.slots[state.timeslots[k]]
+                                  ? Colors.selectedText
+                                  : timeOptionColor,
+                            }}>
+                            {state.timeslots[k]}
+                          </Text>
+                        ))}
+                    </View>
+                  </View>
+                ))
+              ) : (
+                <View>
+                  <Text style={style.specialArrangementText}>是日休息</Text>
+                </View>
+              )}
+            </ScrollView>
+          </View>
+          <View style={style.buttonContainer}>
+            <Button
+              color="white"
+              style={{
+                ...style.button,
+                borderTopRightRadius: 10,
+                borderBottomRightRadius: 10,
+              }}
+              onPress={book.bind(this, false)}>
+              <Text style={style.buttonText}>加入購物車</Text>
+            </Button>
+            <Button
+              color="white"
+              style={{
+                ...style.button,
+                borderTopLeftRadius: 10,
+                borderBottomLeftRadius: 10,
+              }}
+              onPress={book.bind(this, true)}>
+              <Text style={style.buttonText}>立即租訂</Text>
+            </Button>
+          </View>
         </View>
-
-      <View style={style.buttonContainer}>
-        <Button
-          color="white"
-          style={style.button}
-          onPress={book.bind(this, false)}
-        >
-          加入購物車
-        </Button>
-        <Button
-          color="white"
-          style={style.button}
-          onPress={book.bind(this, true)}
-        >
-          立即租訂
-        </Button>
-      </View>
+      </ScrollView>
     </View>
-
   );
 };
 
+const containerBackgroundColor = '#9BC4D8';
+const timeOptionColor = '#9BC4D8';
+
 const style = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-start',
 
     padding: 20,
-    paddingTop: 30
+    paddingTop: 30,
   },
-  roomname: {
-    fontSize: 30
+  header: {
+    backgroundColor: containerBackgroundColor,
+    paddingTop: 5,
+    paddingBottom: 10,
+    marginBottom: 15,
   },
-  header2: {
-    fontSize: 16,
-    color: '#245499',
-    marginTop: 8
+  name: {
+    fontSize: 28,
+    textAlign: 'center',
+  },
+  address: {
+    fontSize: 14,
+    letterSpacing: 2,
+    textAlign: 'center',
+    marginBottom: 3,
+  },
+  price: {
+    fontSize: 10,
+    textAlign: 'center',
+  },
+  datePickerContainer: {
+    marginVertical: 8,
+    marginHorizontal: 30,
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: '300',
+    letterSpacing: 5,
+  },
+  specialArrangementText: {
+    marginHorizontal: 30,
+    marginVertical: 7,
+    fontWeight: '500',
   },
   calendar: {
     // height: 300
-    margin: 10
+    margin: 10,
+  },
+  timePickerContainer: {
+    flex: 1,
+    flexGrow: 1,
+    marginTop: 12,
+  },
+  timePickerOptions: {
+    flex: 0,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginHorizontal: 20,
+    marginBottom: 10,
   },
   scrollContainer: {
     height: 80,
-    backgroundColor: 'red'
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    // justifyContent: 'flex-end',
-    // marginTop: 'auto',
-    padding: 16
+    backgroundColor: 'red',
   },
   ampm: {
     width: '100%',
-    fontSize: 18,
-    marginTop: 8
+    color: 'white',
+    backgroundColor: '#2560A4',
+    fontSize: 13,
+    paddingVertical: 8,
+    paddingLeft: 15,
+    marginBottom: 11,
   },
   half: {
     flexDirection: 'row',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
   },
   text: {
     fontSize: 20,
-    marginRight: 4
+    marginRight: 4,
   },
   slottext: {
-    fontSize: 20,
-    marginRight: 4
+    width: 58,
+    height: 23,
+    overflow: 'hidden',
+    borderColor: timeOptionColor,
+    borderRadius: 7,
+    color: 'white',
+    fontSize: 12,
+    fontWeight: '500',
+    lineHeight: 22,
+    textAlign: 'center',
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 15,
   },
   button: {
     color: 'white',
     backgroundColor: '#225599',
-    width: '40%',
-    height: 40,
-    alignItems: 'center',
-
-    marginHorizontal: 20
-  }
+    width: '35%',
+    height: 35,
+  },
+  buttonText: {
+    fontSize: 12,
+  },
 });
 
 export default Screen;
