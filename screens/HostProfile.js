9909948fead45d933dc6cb183213c3da19c1de3f
Andrew Pang <thpangab@gmail.com>
2020-01-29 13:12:35 +0800

    Add logout alert

--- screens/HostProfile.js
+++ screens/HostProfile.js
@@ -3,17 +3,31 @@ import NavigationService from '../NavigationService';
 import React from 'react';
 import { useStateValue } from '../state';
 
-import { View, StyleSheet } from 'react-native';
+import { Alert, View, StyleSheet } from 'react-native';
 import { Title, Text, Button } from 'react-native-paper';
 import { logout } from '../utils';
 import styles, { Colors } from './Styles';
 
 const Screen = props => {
   const [{ auth }, dispatch] = useStateValue();
-  function onLogout() {
+
+  const logoutAlert = () => {
+    Alert.alert(
+      '確定登出？',
+      '你可以在登入頁面再次登入',
+      [
+        { text: '取消', style: 'cancel' },
+        { text: '確定', onPress: onLogout },
+      ],
+      { cancelable: false },
+    );
+  };
+
+  const onLogout = () => {
     logout(dispatch);
     NavigationService.navigate('GuestFlow');
-  }
+  };
+
   return (
     <View style={styles.container}>
       <Title style={style.header}>使用者資料</Title>
@@ -22,10 +36,7 @@ const Screen = props => {
         <Text style={style.phone}>{'電話：' + auth.tel}</Text>
         <Text style={style.email}>{'帳號電郵：' + auth.email}</Text>
       </View>
-      <Button
-        style={style.logoutButton}
-        mode="outlined"
-        onPress={onLogout.bind(this)}>
+      <Button style={style.logoutButton} mode="outlined" onPress={logoutAlert}>
         <Text style={style.buttonText}>登出</Text>
       </Button>
     </View>
