b4c3161688a1bccfad824b7efd7d6e09eb10a00b
Andrew Pang <thpangab@gmail.com>
2020-01-31 23:16:47 +0800

    update chatroom landing page ui

--- screens/ChatList.js
+++ screens/ChatList.js
@@ -23,7 +23,7 @@ import {
   Text,
   Paragraph,
   List,
-  Checkbox
+  Checkbox,
 } from 'react-native-paper';
 
 // import { KeyboardAwareScrollView as ScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -31,69 +31,66 @@ import styles, { Colors } from './Styles';
 import { getChatroomsForUser, formatTimestamp } from '../utils';
 
 const Screen = props => {
-
   const [unreg, setUnreg] = useState(null);
 
   const [{ auth, chatrooms, openChat }, dispatch] = useStateValue();
 
   const params = props.navigation.state.params;
 
-  useEffect(() => {
+  // useEffect(() => {
+  // getChatroomsForUser().then(list => {
+  //   let ref = firebase
+  //     .firestore()
+  //     .collection('chatrooms')
+  //     ;
 
-    // getChatroomsForUser().then(list => {
-    //   let ref = firebase
-    //     .firestore()
-    //     .collection('chatrooms')
-    //     ;
+  //   if (auth.isHost) {
+  //     ref = ref.where('hostId', '==', auth.id)
+  //   }
+  //   else {
+  //     ref = ref.where('userId', '==', auth.id)
+  //   }
+  //   let xxx = ref.onSnapshot(querySnapshot => {
+  //     let data = [];
+  //     let modified = [];
+  //     querySnapshot.docChanges.forEach(change => {
+  //       let x = change.doc.data();
+  //       if (change.type === 'added') {
+  //         data.push({ id: change.doc.id, ...x });
+  //       } else if (change.type === 'modified') {
+  //         modified.push({ id: change.doc.id, ...x });
+  //       }
+  //     });
+  //     dispatch({
+  //       type: 'chatrooms',
+  //       data,
+  //       modified
+  //     });
+  //   });
+  //   dispatch({ type: 'chatroomsSubscription', data: { sub: xxx } });
+  //   let ret = ref
+  //     .get()
+  //     .then(sp => sp.docs.map(doc => ({ id: doc.id, ...doc.data() })))
+  //     .then(data => {
+  //     })
+  // });
+  // return () => {
+  //   // console.warn('cleanup')
+  //   dispatch({
+  //     type: 'resetChatrooms',
+  //   });
+  // if (unreg != null) {
+  //   unreg.xxx();
+  // }
+  //   };
+  // }, [dispatch]);
 
-    //   if (auth.isHost) {
-    //     ref = ref.where('hostId', '==', auth.id)
-    //   }
-    //   else {
-    //     ref = ref.where('userId', '==', auth.id)
-    //   }
-    //   let xxx = ref.onSnapshot(querySnapshot => {
-    //     let data = [];
-    //     let modified = [];
-    //     querySnapshot.docChanges.forEach(change => {
-    //       let x = change.doc.data();
-    //       if (change.type === 'added') {
-    //         data.push({ id: change.doc.id, ...x });
-    //       } else if (change.type === 'modified') {
-    //         modified.push({ id: change.doc.id, ...x });
-    //       }
-    //     });
-    //     dispatch({
-    //       type: 'chatrooms',
-    //       data,
-    //       modified
-    //     });
-    //   });
-    //   dispatch({ type: 'chatroomsSubscription', data: { sub: xxx } });
-    //   let ret = ref
-    //     .get()
-    //     .then(sp => sp.docs.map(doc => ({ id: doc.id, ...doc.data() })))
-    //     .then(data => {
-    //     })
-    // });
-    return () => {
-      // console.warn('cleanup')
-      dispatch({
-        type: 'resetChatrooms'
-      });
-      // if (unreg != null) {
-      //   unreg.xxx();
-      // }
-    }
-  }, []);
-
-
-  const navigateChat = (x) => {
+  const navigateChat = x => {
     NavigationService.navigate('Chat', x);
-  }
+  };
 
   return (
-    <View style={styles.container}>
+    <View style={style.container}>
       <NavigationEvents
         onWillFocus={payload => {
           // console.warn('aa', openChat)
@@ -103,7 +100,6 @@ const Screen = props => {
               NavigationService.navigate('Chat', openChat);
             }
           }, 1000);
-
         }}
         onWillBlur={payload => {
           // console.warn('unreg chatlist')
@@ -114,65 +110,110 @@ const Screen = props => {
           //   unreg.xxx();
           // }
         }}
-
       />
-      <Title style={style.title}>談話</Title>
-      <View style={styles.headerBar}></View>
+      <Title style={style.title}>對話</Title>
       {/* <ScrollView> */}
       <FlatList
         initialNumToRender={0}
         data={chatrooms}
         keyExtractor={(item, index) => item.id}
-        contentContainerStyle={{ paddingLeft: 10, paddingRight: 10 }}
-        ItemSeparatorComponent={() => (<View style={{
-          height: 1,
-          backgroundColor: '#999999',
-          width: "100%",
-        }}></View>)}
+        ItemSeparatorComponent={() => <View style={style.messageDivider} />}
         renderItem={({ item }) => (
           <View>
             <TouchableHighlight onPress={() => navigateChat(item)}>
               <View style={style.messageBlock}>
-                <Text style={style.name}>{auth.isHost ? item.user : item.host}</Text>
-                <View style={{ ...styles.row, justifyContent: 'space-between' }}>
-                  {(auth.isHost ? item.hostNewMessage : item.userNewMessage) > 0 ?
-                    (<View style={style.newMessage}>
-                      <Text style={{ color: 'white', textAlign: 'center' }}>{(auth.isHost ? item.hostNewMessage : item.userNewMessage)}</Text>
-                    </View>) : null}
-                  <Text style={style.message} numberOfLines={1} ellipsizeMode="tail">{item.lastMessage}</Text>
-                  <Text style={style.time}>{formatTimestamp(item.lastMessageTime)}</Text>
+                <Text style={style.name}>
+                  {auth.isHost ? item.user : item.host}
+                </Text>
+                <View style={style.messageGroup}>
+                  <View style={style.messageContent}>
+                    {(auth.isHost ? item.hostNewMessage : item.userNewMessage) >
+                    0 ? (
+                      <View style={style.newMessageIndicator}>
+                        <Text style={style.newMessageCount}>
+                          {auth.isHost
+                            ? item.hostNewMessage
+                            : item.userNewMessage}
+                        </Text>
+                      </View>
+                    ) : null}
+                    <Text
+                      style={style.message}
+                      numberOfLines={1}
+                      ellipsizeMode="tail">
+                      {item.lastMessage}
+                    </Text>
+                  </View>
+                  <Text style={style.timestamp}>
+                    最後訊息時間{formatTimestamp(item.lastMessageTime)}
+                  </Text>
                 </View>
               </View>
             </TouchableHighlight>
           </View>
-        )} />
+        )}
+      />
       {/* </ScrollView> */}
     </View>
   );
 };
 
 const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    paddingTop: 10,
+  },
   title: {
-    padding: 10
+    color: Colors.title,
+    marginLeft: 30,
+    marginBottom: 8,
+  },
+  messageDivider: {
+    height: 2,
+    backgroundColor: 'white',
+    width: '100%',
   },
   messageBlock: {
-    padding: 8
+    paddingVertical: 8,
+    paddingHorizontal: 30,
+    backgroundColor: '#2560A4',
+  },
+  messageGroup: {
+    ...styles.row,
+    justifyContent: 'space-between',
+  },
+  messageContent: {
+    ...styles.row,
+    justifyContent: 'flex-start',
   },
   name: {
-    fontWeight: 'bold'
+    marginBottom: 2,
+    fontSize: 16,
+    letterSpacing: 2,
+    color: '#9BC4D8',
   },
-  newMessage: {
-    borderRadius: 4,
+  newMessageIndicator: {
+    marginRight: 5,
+    width: 15,
+    borderRadius: 5,
     backgroundColor: 'red',
-    width: 20
+  },
+  newMessageCount: {
+    color: 'white',
+    fontSize: 11,
+    textAlign: 'center',
   },
   message: {
-    width: '70%'
-
+    width: '60%',
+    fontSize: 11,
+    color: 'white',
   },
-  time: {
-
-  }
+  timestamp: {
+    fontSize: 11,
+    letterSpacing: 1,
+    color: 'white',
+  },
 });
 
 export default Screen;
